55a2a3e03f51521244c0088b4b8e2478cc84aa09
Giang Streamy <giang.nguyen@streamy.live>
2019-04-05 22:09:14 +0700

    update css

--- wp-content/plugins/wpforms-ajax-submit/wpforms-ajax-submit9030.js
+++ wp-content/plugins/wpforms-ajax-submit/wpforms-ajax-submit9030.js
@@ -29,4 +29,31 @@
         $('.loading').hide();
         return false;
     });
+
+   var submit = $("#wpforms-submit-9-popup");
+    submit.click(function(){
+        $('.loading').show();
+        var form = submit.closest("form");
+        data = form.serialize();
+        
+        $.ajax({
+            type : 'GET', 
+            url : 'https://script.google.com/macros/s/AKfycbzOXhF63KgjnSLaW-DjbKCxB-To_w3ebAJ3Bt5u97U8cSzYqkk/exec',
+            dataType:'json',
+            crossDomain : true,
+            data : data,
+            success : function(data) {
+              if(data == 'false') 
+              {
+                 alert('đăng kí không thành công xin vui lòng thử ');
+              }else{
+                 alert('Đã đăng kí thành công');
+              }
+            }
+        });
+        $('#mask').hide();
+        $('.window').hide();
+        $('.loading').hide();
+        return false;
+    });
 	});
